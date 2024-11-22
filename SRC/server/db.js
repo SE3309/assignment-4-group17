6e95be053e993b67cc02ff722a93e6575c24df33
DexterLuth <dluth@uwo.ca>
2024-11-22 11:16:08 -0500

    connected mysql

---
+++ SRC/server/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "test",
+    database: 'se3309'
+  
+  });
+  
+
+  module.exports =con
--- SRC/server/server.js
+++ SRC/server/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const con = require('./db')
+
+
+con.query(`SELECT * FROM conditions`, function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
